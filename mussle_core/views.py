418d029b7b91a93592f85465a3247140e1a13dd2
popsuzuki <whereismygreendoor666@gmail.com>
2018-06-02 17:09:59 +0900

    #1 凡庸ビューを活用したページ作成中
    リストはテンプレートビューとリストビューを採用

--- mussle_core/views.py
+++ mussle_core/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from django.views.generic import *
+from django.views.generic.edit import FormMixin, DeletionMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import *
+from mussle_core.models import Musslemenu
 
 
-def index(request):
-    return render(request, 'portal.html')
+class MussleList(TemplateView, ListView):
+    template_name = 'mussle_list.html'
+    model = Musslemenu
+
+    def get_context_data(self, **kwargs):
+        context = super(MussleList, self).get_context_data(**kwargs)
+        context['mussle_lists'] = Musslemenu.objects.all()
+        return context
